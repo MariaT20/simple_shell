e6e80f8804aec4aa1e5c72ce2327c9156da257fe
ayoolaadewale <ayooladewale@yahoo.com>
2022-08-05 01:12:27 +0000

    Betty code

---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+#include <dirent.h>
+#include <errno.h>
+#include <fctnl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stlib.h>
+#include <sys/resource.h>
